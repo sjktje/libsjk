a1c3b0ef2f1ff3ceec3dcee079e92d71d275631d
Svante Kvarnstrom <sjk@ankeborg.nu>
2010-01-13 23:29:36 +0100

    Forgot to add time.h include

--- libsjk.c
+++ libsjk.c
@@ -1,8 +1,9 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 #include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "libsjk.h"
 
